9db9b5944b6c2134583a0770b58c91867df207fc
Anita Lee <anita.lee@gmail.com>
2022-03-15 20:50:56 -0400

    upload completed ajax assignment

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log("Let's get this party started!");
+
+// create a search input box that takes type=text
+
+// two buttons
+// one button to start AJAX request - searchGiphy
+// one botton to delete all gifs (clear page) - removeImages
+
+// DATA FUNCTION: create AJAX request that checks GIPHY api for data/url
+async function searchGiphy() {
+  let searchTerm = $("input").val();
+  console.log("searchTerm=", searchTerm);
+
+  let response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: { q: searchTerm, api_key: "vOTSwQwR5j9zIAJmNn7NG2FqZotgdPrL" },
+  });
+  console.log("searchGiphy response data =", response);
+
+  const randomIndex = randomInt(50);
+
+  //   console.log(response.data.data.randomIndex.images.original.url);
+  appendGif(response.data.data[randomIndex].images.original.url);
+}
+
+$("#search").on("click", searchGiphy);
+
+$("#remove").on("click", clearGifs);
+
+// DOM FUNCTION: takes the data/url from DATA FUNCTIOn and append gif to the page
+
+function appendGif(url) {
+  let $results = $("#results");
+  let $image = $(`<img src="${url}"> `);
+
+  console.log("$image=", $image);
+
+  $results.append($image);
+  $("input").val("");
+}
+
+function clearGifs() {
+  $("#results").empty();
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
